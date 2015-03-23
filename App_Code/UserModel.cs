c0a7c54421649b2ccd5e2df301646c26a0c33c4f
Andrey King <b.golicin@gmail.com>
2015-03-23 14:45:22 +0300

    1

---
+++ App_Code/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserModel
+/// </summary>
+internal abstract class UserModel
+{
+    public ulong ID { get; private set; }
+    public string Nickname { get; private set; }
+    public string
+    public string FirstName { get; private set; }
+    public string SecondName { get; private set; }
+    public DateTime BirthDate { get; private set; }
+    public string Avatar { get; private set; }
+    public uint Balance { get; private set; }
+    public ushort Stage { get; private set; }
+    public bool IsLogined { get; private set; }
+    public Sex Gender { get; private set; }
+    public enum Sex
+    {
+        Male, Female
+    }
+    
+
+    public UserModel()
+    {
+
+    }
+}
